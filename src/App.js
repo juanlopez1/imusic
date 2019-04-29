48278fe6f3bebd6e2ca0d7eab9fee6829cdb3a11
juanlopez1 <juan1manuel9lopez96@gmail.com>
2019-04-29 01:00:32 -0300

    feat(eslint): add eslint and configure rules

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const text = 'src/App.js';
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit
+                    &nbsp;
+                    <code>{text}</code>
+                    &nbsp;
+                    and save to reload
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </p>
+            </header>
+        </div>
+    );
+};
 
 export default App;
